76afb71f50be508ea9650f9a3a11d5f8fb6e32d1
村尾 一樹 <t2052256@kita9.ed.jp>
2025-03-18 12:22:14 +0900

    前のやつ

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //タイトルテキストアニメーション
 const animationTargetElements = document.querySelectorAll(".textanimation");
 for( let i =0; i < animationTargetElements.length; i++ ){
@@ -19,26 +18,4 @@ for( let i =0; i < animationTargetElements.length; i++ ){
 const cursor = document.getElementById("cursor");
 document.addEventListener("mousemove",function(e){
     cursor.style.transform =  ` translate(${e.clientX}px,${e.clientY}px) ` 
-=======
-//タイトルテキストアニメーション
-const animationTargetElements = document.querySelectorAll(".textanimation");
-for( let i =0; i < animationTargetElements.length; i++ ){
-    const targetElement = animationTargetElements[i]
-    texts = targetElement.textContent;
-    textsArray = [];
-    
-    targetElement.textContent = "";
-
-
-    for(let j =0; j < texts.split("").length; j++){
-    textsArray.push('<span><span style="animation-delay: '+ ((j+5) * 0.2) +'s" >' + texts.split("")[j] + '<span></span>')
-    }
-    for(let k =0; k < textsArray.length; k++){
-        targetElement.innerHTML += textsArray[k];
-    }
-}
-const cursor = document.getElementById("cursor");
-document.addEventListener("mousemove",function(e){
-    cursor.style.transform =  ` translate(${e.clientX}px,${e.clientY}px) ` 
->>>>>>> origin/main
 });
