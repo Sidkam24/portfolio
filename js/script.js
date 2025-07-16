30db2e7a223c01d9d77be2efc3d020565a33efd9
unknown <t2052256@kita9.ed.jp>
2025-07-16 12:33:36 +0900

    ハンバーガーメニュー途中2

--- js/script.js
+++ js/script.js
@@ -1,21 +1,31 @@
 //タイトルテキストアニメーション
-const animationTargetElements = document.querySelectorAll(".textanimation");
-for( let i =0; i < animationTargetElements.length; i++ ){
-    const targetElement = animationTargetElements[i]
-    texts = targetElement.textContent;
-    textsArray = [];
+$(function(){
+// const animationTargetElements = document.querySelectorAll(".textanimation");
+// for( let i =0; i < animationTargetElements.length; i++ ){
+//     const targetElement = animationTargetElements[i]
+//     texts = targetElement.textContent;
+//     textsArray = [];
     
-    targetElement.textContent = "";
+//     targetElement.textContent = "";
 
 
-    for(let j =0; j < texts.split("").length; j++){
-    textsArray.push('<span><span style="animation-delay: '+ ((j+5) * 0.2) +'s" >' + texts.split("")[j] + '<span></span>')
-    }
-    for(let k =0; k < textsArray.length; k++){
-        targetElement.innerHTML += textsArray[k];
-    }
-}
-const cursor = document.getElementById("cursor");
-document.addEventListener("mousemove",function(e){
-    cursor.style.transform =  ` translate(${e.clientX}px,${e.clientY}px) ` 
+//     for(let j =0; j < texts.split("").length; j++){
+//     textsArray.push('<span><span style="animation-delay: '+ ((j+5) * 0.2) +'s" >' + texts.split("")[j] + '<span></span>')
+//     }
+//     for(let k =0; k < textsArray.length; k++){
+//         targetElement.innerHTML += textsArray[k];
+//     }
+// }
+// const cursor = document.getElementById("cursor");
+// document.addEventListener("mousemove",function(e){
+//     cursor.style.transform =  ` translate(${e.clientX}px,${e.clientY}px) ` 
+// });
+
+
+//ハンバーガーメニュー
+$(".hamburger").on("click",function(){
+    $(".header_nav ul").toggleClass("nav_toggle");
+    $(".header_nav ul").css("display","flex");
+});
+
 });
