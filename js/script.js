773fc3979cf233763ece4ad4bc6fbe55da8a5438
unknown <t2052256@kita9.ed.jp>
2025-07-17 10:34:44 +0900

    ハンバーガーメニュー

--- js/script.js
+++ js/script.js
@@ -21,11 +21,24 @@ $(function(){
 //     cursor.style.transform =  ` translate(${e.clientX}px,${e.clientY}px) ` 
 // });
 
-
+let hamburger_click_count = 0;
 //ハンバーガーメニュー
 $(".hamburger").on("click",function(){
+    $(".hamburger").toggleClass("toggle");
     $(".header_nav ul").toggleClass("nav_toggle");
-    $(".header_nav ul").css("display","flex");
+    hamburger_click_count++;
+    even_count = hamburger_click_count%2;
+    if(even_count == 0){
+        $(".header_nav ul").css("animation","close-circle 1s forwards");
+    }else{
+        $(".header_nav ul").css("animation","circle 1s");
+        $(".header_nav ul").css("display","flex");
+    }
+});
+$(".header_ul li a").on("click",function(){
+    $(".header_nav ul").css("animation","close-circle 1s forwards");
+    $(".hamburger").removeClass("toggle");
+    hamburger_click_count++;
 });
 
 });
